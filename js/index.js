11aa81f8d014860222ddd564128b9b6c9850a8fe
abrorjon016 <ikromjonovabrorjon3@gmail.com>
2024-10-01 23:35:48 +0500

    hi

---
+++ js/index.js
@@ -0,0 +1,229 @@
+// 1-masala
+// let num = +prompt("sonni kiriting");
+// let res;
+
+// if (num > 0) {
+//     res = num += 1;
+// }
+// else{
+//     res = num;
+// }
+// console.log(res);
+
+// 2-masala
+// let num = +prompt("sonni kiriting");
+// let res;
+
+// if (num > 0) {
+//     ret = num += 1;
+// }
+// else{
+//     ret = num -= 2;
+// }
+// console.log(res);
+
+// 3 - masala
+// let num = +prompt("sonni kiriting");
+// let ret;
+
+// if (num > 0) {
+//     res = num += 1;
+// }
+// else if (son < 0) {
+//     res = num -= 2;
+// }
+// else{
+//     res = son = 10;
+// }
+// console.log(res);
+
+// 4-masala
+
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let num3 = +prompt("3-sonni kiriting");
+// let musbat = 0 ;
+// if (num1 > 0) {
+//     musbat++;
+// }
+// if (num2 > 0) {
+//     musbat++;
+// }
+// if (num3 > 0) {
+//     musbat++;
+// }
+// console.log("musbat");
+
+// 5-masala
+
+// 6-masala
+
+// let num1 = +prompt("1-sonni kiriting");
+// let son2 = +prompt("2-sonni kiriting");
+// let res ;
+
+// if (num1 > num2) {
+//     res = num
+// }
+// else{
+//     res = num2
+// }
+// console.log(res);
+
+// 7-masala
+
+// let num1 = +prompt("1-sonni kiriting");
+// let son2 = +prompt("2-sonni kiriting");
+// let kichik;
+// let index;
+
+// if (num1 < num2) {
+//     kichik = num1;
+//     index = 1;
+// } else {
+//     kichik = num2;
+//     index = 2;
+// }
+// console.log( index);
+
+//8-masala
+
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let katta;
+// let kichik;
+
+// if (num1 < num2) {
+//     kichik = num1;
+//     katta = son2;
+// } else {
+//     kichik = num2;
+//     katta = num1;
+// }
+// console.log('kottasi: ' + katta + ' ' + 'kichigi: ' + kichik);
+
+//9-masala
+
+// 10 masala
+
+//11-masala
+
+// 13 - masala
+
+//  13, 14, 15 ni ertaga ishlab push qilib qoyaman, ishlay olmadim
+
+// Boolean
+
+// 1-masala
+// let a = 12;
+// let res;
+
+// if (a > 0) {
+//     res = true
+// }
+// else{
+//     res = false
+// }
+// console.log(res);
+
+//2-masala
+
+// let a = 12;
+// let res;
+
+// if (a % 2 == 1) {
+//     res = true
+// }
+// else{
+//     res = false
+// }
+// console.log(res);
+
+//3-masala
+
+// let a = 12;
+// let res;
+
+// if (a % 2 == 0) {
+//     res = true
+// }
+// else{
+//     res = false
+// }
+// console.log(res);
+
+// 4-masala
+
+// let a = 12;
+// let b = 12;
+// let res;
+
+// if (a > 2 && b <= 3) {
+//     res = true
+// }
+// else{
+//     res = false
+// }
+// console.log(res);
+
+// 5-masala
+
+// 6-masala
+
+// let a = 12;
+// let b = 12;
+// let c = 12;
+// let res;
+
+// if (a <= b <= c) {
+//     res = true
+// }
+// else{
+//     res = false
+// }
+// console.log(res);
+
+// 7-masala
+
+// let a = 12;
+// let b = 12;
+// let c = 12;
+// let res;
+
+// if (a <= c <= b) {
+//     res = true
+// }
+// else{
+//     res = false
+// }
+// console.log(res);
+
+// 8-masala
+
+// 9-masala
+
+// let a = 12;
+// let b = 12;
+// let res;
+
+// if (a % 2 == 1 || b % 2 == 1) {
+//     res = true
+// }
+// else{
+//     res = false
+// }
+// console.log(res);
+
+//10-masala
+
+// let a = 12;
+// let b = 12;
+// let res;
+
+// if (a % 2 == 1 && b % 2 == 1) {
+//     res = true
+// }
+// else{
+//     res = false
+// }
+// console.log(res);
